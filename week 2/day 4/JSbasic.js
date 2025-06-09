8270906bf37e89246c6176ff2918d8f8209a8e02
akshay <shettyakshay333@gmail.com>
2025-06-09 22:27:30 +0530

    day 4

---
+++ week 2/day 4/JSbasic.js
@@ -0,0 +1,23 @@
+// Harkirath course  JS BASICS
+var name = "Akshay ";
+console.log(" hellp ", name);
+
+name = true;
+console.log(" hellp ", name);
+
+name = 5;
+console.log(" hellp ", name);
+
+console.log(" hellp ", name);
+
+function printt(name) {
+  console.log(" hello ", name);
+}
+
+printt("akshay");
+
+nam = ["akshay", "dk", "vk", " Kl ", "Jitesh ", " Rapa "];
+while (1) {
+  num = Math.floor(Math.random(0, nam.length - 1) * 10);
+  printt(nam[num], " ", num);
+}
