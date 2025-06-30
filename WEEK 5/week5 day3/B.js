7c10f44dd82cb7b02d1b7f36830595a691f4396e
akshay <shettyakshay333@gmail.com>
2025-06-30 22:18:46 +0530

    UpdatedB

--- WEEK 5/week5 day3/B.js
+++ WEEK 5/week5 day3/B.js
@@ -2,12 +2,16 @@ function setTimoutPromisified(ms) {
   return new Promise((resolve) => {
     setTimeout(() => {
       console.log(" hello");
-      resolve()
+      resolve();
     }, ms);
   });
 }
 
 function callback() {
   console.log(" resolved");
+  setTimeout((resolve) => {
+    console.log(" hi");
+    resolve();
+  }, 2000);
 }
 setTimoutPromisified(1000).then(callback);
