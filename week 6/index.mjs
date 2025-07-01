32d3444412e8a7abcda17b3cb738c12a28cbd071
akshay <shettyakshay333@gmail.com>
2025-07-01 21:51:35 +0530

    Updated_code_and _into_to_MJS

---
+++ week 6/index.mjs
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+console.log(chalk.inverse("hello World !"));
+
+console.log(chalk.red.bold("this is hello from mjs "));
+
+
+
+console.log(chalk.blue.italic("this is hello from mjs "));
