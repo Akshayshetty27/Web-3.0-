e17d22b2d5f969c9bb0f51743ce01137a8b1abb4
akshay <shettyakshay333@gmail.com>
2025-07-02 22:23:12 +0530

    Updated_read

--- week 6/Week6_day2/Read.js
+++ week 6/Week6_day2/Read.js
@@ -1,17 +1,22 @@
 let r = require("fs");
+let path = require("path");
 
 function read(filename) {
-  r.readFile(filename,"utf-8", function (err, data) {
+  r.readFile(filename, "utf-8", function (err, data) {
     let total = 0;
-
+    let temp = "";
     for (let i = 0; i < data.length; i++) {
+      temp += data[i];
       if (data[i] === " ") {
+        console.log(temp);
+        __dirname += temp;
+        temp = "";
         total++;
       }
     }
     console.log(total);
+    console.log("  " + __dirname);
   });
 }
 
-
-read("a.txt")
+read("a.txt");
